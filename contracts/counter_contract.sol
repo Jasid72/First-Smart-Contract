0d2aaa69d818a2421c5e193d2cfec990ffc3b15e
Jasid72 <Jasidhassan@gmail.com>
2024-08-26 04:11:47 +0500

    Counter Contract Added

---
+++ contracts/counter_contract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter{
+    uint public  count;
+    function increment() public {
+        count += 1;
+    }
+    function decrement() public {
+        count -= 1;
+    }
+}
--- contracts/hello.sol
+++ contracts/hello.sol
@@ -5,4 +5,9 @@ contract HelloWorld{
     {
         return "This is my First Contarct";
     }//Pure  doesn't read or modify any state variables of the contract.
+    string message = "This Message for Expalin the view."; //Right now this variable is decalare globally. If it is decalare inside the function then it is knows as locally scope declare variable.
+    function mess() public view returns(string memory) // View is only use for read Data.
+    {
+        return message;
+    }
 }
